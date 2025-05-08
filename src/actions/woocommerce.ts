5fddeacea1faa8d6e048ceffe42ddb2cc5d97643
duncan-nn <nnoromduncan@gmail.com>
2025-05-08 02:30:00 +0100

    11 commit

--- src/actions/woocommerce.ts
+++ src/actions/woocommerce.ts
@@ -3,12 +3,6 @@
 import { Product } from '@/utils/models';
 import axios from 'axios';
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
 export const getProducts = async () => {
   try {
     const response = await axios.get('https://lisafolawiyo.com/wp-json/wc/v3/products?consumer_key='+process.env.WC_CONSUMER_KEY+'&consumer_secret='+process.env.WC_CONSUMER_SECRET+'&per_page=12&page=1');
